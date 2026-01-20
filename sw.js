9fbaf3e6df0a2834754f73c2068c2025e7525baa
T-Rogo <Rogotzketravis@gmail.com>
2026-01-19 19:01:35 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "trackjack-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
